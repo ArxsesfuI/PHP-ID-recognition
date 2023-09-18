b5138de2f6170095d171eb3ee6d07031dd6a5ff7
Bartek <54020563+ArxsesfuI@users.noreply.github.com>
2023-09-18 19:42:58 +0200

    Add files via upload

---
+++ game.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="game.css">
+        <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    </head>
+    <body>
+        <?php
+            $id = $_GET['id'];
+            
+            if ($id == 1){
+                echo "<img src='img/minecraft-icon.png'>";
+                echo "<h2>Minecraft is a sandbox.</h2>";
+                echo "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum a sapien sed faucibus. Vestibulum aliquet eleifend maximus. Integer ipsum dui, commodo eu dui ac, scelerisque porta justo. Praesent aliquam, ipsum eget cursus interdum, felis sem porttitor augue, vitae scelerisque sapien lacus eget risus. Donec non est eget purus dictum consequat in ut dui. Sed mattis lorem ac ligula dictum, scelerisque pulvinar quam dictum. Curabitur porttitor, velit quis lobortis tristique, ligula metus lobortis enim, nec condimentum nisl ante sed neque. Aenean efficitur condimentum facilisis. Sed mauris dui, vehicula nec semper in, porttitor in augue. Maecenas porttitor massa vitae lectus posuere varius. Donec eleifend nibh in odio laoreet maximus.
+
+                Sed laoreet sagittis dictum. Sed viverra felis eu blandit ullamcorper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis ultricies augue lacus, a consectetur elit vulputate semper. Aliquam erat volutpat. Quisque lacinia placerat ante in placerat. Etiam fringilla erat nec mi rutrum, eget ultrices ipsum fringilla. Mauris varius nec magna lacinia sodales.</p>";
+            } elseif ($id == 2){
+                echo "<img src='img/fortnite-icon.png'>";
+                echo "<h2>Fortnite is a battle royale.</h2>";
+                echo "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum a sapien sed faucibus. Vestibulum aliquet eleifend maximus. Integer ipsum dui, commodo eu dui ac, scelerisque porta justo. Praesent aliquam, ipsum eget cursus interdum, felis sem porttitor augue, vitae scelerisque sapien lacus eget risus. Donec non est eget purus dictum consequat in ut dui. Sed mattis lorem ac ligula dictum, scelerisque pulvinar quam dictum. Curabitur porttitor, velit quis lobortis tristique, ligula metus lobortis enim, nec condimentum nisl ante sed neque. Aenean efficitur condimentum facilisis. Sed mauris dui, vehicula nec semper in, porttitor in augue. Maecenas porttitor massa vitae lectus posuere varius. Donec eleifend nibh in odio laoreet maximus.
+
+                Sed laoreet sagittis dictum. Sed viverra felis eu blandit ullamcorper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis ultricies augue lacus, a consectetur elit vulputate semper. Aliquam erat volutpat. Quisque lacinia placerat ante in placerat. Etiam fringilla erat nec mi rutrum, eget ultrices ipsum fringilla. Mauris varius nec magna lacinia sodales.</p>";
+            } elseif ($id == 3){
+                echo "<img src='img/lol-icon.png'>";
+                echo "<h2>League of Legends is a MOBA.</h2>";
+                echo "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum a sapien sed faucibus. Vestibulum aliquet eleifend maximus. Integer ipsum dui, commodo eu dui ac, scelerisque porta justo. Praesent aliquam, ipsum eget cursus interdum, felis sem porttitor augue, vitae scelerisque sapien lacus eget risus. Donec non est eget purus dictum consequat in ut dui. Sed mattis lorem ac ligula dictum, scelerisque pulvinar quam dictum. Curabitur porttitor, velit quis lobortis tristique, ligula metus lobortis enim, nec condimentum nisl ante sed neque. Aenean efficitur condimentum facilisis. Sed mauris dui, vehicula nec semper in, porttitor in augue. Maecenas porttitor massa vitae lectus posuere varius. Donec eleifend nibh in odio laoreet maximus.
+
+                Sed laoreet sagittis dictum. Sed viverra felis eu blandit ullamcorper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis ultricies augue lacus, a consectetur elit vulputate semper. Aliquam erat volutpat. Quisque lacinia placerat ante in placerat. Etiam fringilla erat nec mi rutrum, eget ultrices ipsum fringilla. Mauris varius nec magna lacinia sodales.</p>";
+            } elseif ($id == 4){
+                echo "<img src='img/witcher3-icon.png'>";
+                echo "<h2>Witcher 3: Wild Hunt is an RPG game.</h2>";
+                echo "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum a sapien sed faucibus. Vestibulum aliquet eleifend maximus. Integer ipsum dui, commodo eu dui ac, scelerisque porta justo. Praesent aliquam, ipsum eget cursus interdum, felis sem porttitor augue, vitae scelerisque sapien lacus eget risus. Donec non est eget purus dictum consequat in ut dui. Sed mattis lorem ac ligula dictum, scelerisque pulvinar quam dictum. Curabitur porttitor, velit quis lobortis tristique, ligula metus lobortis enim, nec condimentum nisl ante sed neque. Aenean efficitur condimentum facilisis. Sed mauris dui, vehicula nec semper in, porttitor in augue. Maecenas porttitor massa vitae lectus posuere varius. Donec eleifend nibh in odio laoreet maximus.
+
+                Sed laoreet sagittis dictum. Sed viverra felis eu blandit ullamcorper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis ultricies augue lacus, a consectetur elit vulputate semper. Aliquam erat volutpat. Quisque lacinia placerat ante in placerat. Etiam fringilla erat nec mi rutrum, eget ultrices ipsum fringilla. Mauris varius nec magna lacinia sodales.</p>";
+            } elseif ($id == 5){
+                echo "<img src='img/xcom2-icon.png'>";
+                echo "<h2>Xcom 2: War of the Chosen is a strategy.</h2>";
+                echo "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum a sapien sed faucibus. Vestibulum aliquet eleifend maximus. Integer ipsum dui, commodo eu dui ac, scelerisque porta justo. Praesent aliquam, ipsum eget cursus interdum, felis sem porttitor augue, vitae scelerisque sapien lacus eget risus. Donec non est eget purus dictum consequat in ut dui. Sed mattis lorem ac ligula dictum, scelerisque pulvinar quam dictum. Curabitur porttitor, velit quis lobortis tristique, ligula metus lobortis enim, nec condimentum nisl ante sed neque. Aenean efficitur condimentum facilisis. Sed mauris dui, vehicula nec semper in, porttitor in augue. Maecenas porttitor massa vitae lectus posuere varius. Donec eleifend nibh in odio laoreet maximus.
+
+                Sed laoreet sagittis dictum. Sed viverra felis eu blandit ullamcorper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis ultricies augue lacus, a consectetur elit vulputate semper. Aliquam erat volutpat. Quisque lacinia placerat ante in placerat. Etiam fringilla erat nec mi rutrum, eget ultrices ipsum fringilla. Mauris varius nec magna lacinia sodales.</p>";
+            } else {
+                echo "<img src='img/minecraft-icon.png'>";
+            }
+            echo "<footer>Arxsesful &copy;2023</footer>";
+        ?>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+    <head>
+    <link rel="stylesheet" href="index.css">
+    <link rel="icon" type="image/x-icon" href="img/favicon.ico">
+    </head>
+    <body>
+        <?php
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "baza gier";
+
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            if ($conn->connect_error) {
+                die("Błąd połączenia z bazą danych: " . $conn->connect_error);
+            }
+
+            // Nazwa tabeli, którą chcesz wyświetlić
+            $tabela = "games";
+
+            // Zapytanie SQL do wyświetlenia danych z tabeli
+            $sql = "SELECT * FROM $tabela";
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                // Wyświetl dane z tabeli
+                echo "<h1>Baza danych gier</h1>";
+                echo "<table>";
+                echo "<tr><th>LP</th><th>Nazwa</th><th>Description</th></tr>";
+                while ($row = $result->fetch_assoc()) {
+                    // Tworzenie linku do każdego rekordu
+                    $id = $row["id"];
+                    $title = $row["title"];
+                    $description = $row["description"];
+                    echo "<tr><td><a href='game.php?id=$id'>$id</a></td><td><a href='game.php?id=$id'>$title</a></td><td><a href='game.php?id=$id'>$description</a></td></tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "Brak danych w tabeli.";
+            }
+
+            // Zakończ połączenie po zakończeniu operacji
+            $conn->close();
+            
+            echo "<h2>Kliknij w grę, aby dowiedzieć się o niej więcej</h2>";
+            echo "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dictum a sapien sed faucibus. Vestibulum aliquet eleifend maximus. Integer ipsum dui, commodo eu dui ac, scelerisque porta justo. Praesent aliquam, ipsum eget cursus interdum, felis sem porttitor augue, vitae scelerisque sapien lacus eget risus. Donec non est eget purus dictum consequat in ut dui. Sed mattis lorem ac ligula dictum, scelerisque pulvinar quam dictum. Curabitur porttitor, velit quis lobortis tristique, ligula metus lobortis enim, nec condimentum nisl ante sed neque. Aenean efficitur condimentum facilisis. Sed mauris dui, vehicula nec semper in, porttitor in augue. Maecenas porttitor massa vitae lectus posuere varius. Donec eleifend nibh in odio laoreet maximus.
+
+            Sed laoreet sagittis dictum. Sed viverra felis eu blandit ullamcorper. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Interdum et malesuada fames ac ante ipsum primis in faucibus. Duis ultricies augue lacus, a consectetur elit vulputate semper. Aliquam erat volutpat. Quisque lacinia placerat ante in placerat. Etiam fringilla erat nec mi rutrum, eget ultrices ipsum fringilla. Mauris varius nec magna lacinia sodales.</p>";
+            echo "<footer>Arxsesful &copy;2023</footer>";
+        ?>
+    </body>
+</html>
